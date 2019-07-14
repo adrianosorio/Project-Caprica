19ff0b4af4b24ea3f22af37757b35206cd86e120
adrianosorio <adrianosorio101@gmail.com>
2019-07-14 16:01:53 -0700

    Bootstrap and Jquery Fix
    Fixed issues with the guide when the page resizes, added in bootstrap and will be testing its capabilities when a new page is created.

--- navbar.js
+++ navbar.js
@@ -2,17 +2,33 @@
 var nav = document.getElementById("cylon-eye");
 
 $(document).ready(function() { //To animate make sure to define position type
+    
+    // The tabs on the side menu
     var tab = $("li");
-    var eye = $("#guide")
+    
+    // The sliding guide to the left of the menu
+    var eye = $("#guide");
+    
+    // Root of the anvigation bar
+    var nav = $("#navigation");
+    
+    // Speed param of hover highlight
     var speed_highlight = 100;
+    
+    // The speed at which the guide will move
     var speed_hover = 350;
+    
+    // The initial spacing 
     var initial_offset = $("li.active").offset();
+    
+    // Moves the guide to the initial position near the active tab
     eye.animate({top: initial_offset.top}, 0);
     $("body").hide().fadeIn(2000);
+    
     //Adding animation to make the greeting fade into the page
     $("#greeting").hide().fadeIn(3000);
     
-    //When we hover over create border bottom
+    //When we hover over a tab create red border bottom
     tab.hover(function() {
         $(this).animate({borderBottomColor: "#fe0606", borderBottomStyle: "solid", borderBottomWidth: "5px"}, speed_highlight);
     }, function() {
@@ -30,6 +46,13 @@ $(document).ready(function() { //To animate make sure to define position type
     tab.mouseleave(function(){
         var offset = $("li.active").offset();
         eye.stop(true, false);//stopped bad queueing
+        eye.animate({top: offset.top}, speed_hover);
+    });
+    
+    //In the case of a Resize event for the nav-bar
+    $(window).resize(function(){
+        var offset = $("li.active").offset();
+        eye.stop(true, false);
         eye.animate({top: offset.top}, speed_hover);
     });
     
